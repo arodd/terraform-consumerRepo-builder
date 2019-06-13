959b491bda24a70948c2b5b5aa1e69b30cb89fd1
Austin Workman <austin@hashicorp.com>
2019-06-12 21:03:44 -0500

    Updating source repo

--- github.tf
+++ github.tf
@@ -1,19 +1,20 @@
-
 data "template_file" "create" {
   template = "${file("create_repo.tmpl")}"
+
   vars = {
-    git_token = "${var.git_token}"
+    git_token    = "${var.git_token}"
     new_git_repo = "${var.new_git_repo}"
-    git_user = "${var.git_user}"
+    git_user     = "${var.git_user}"
   }
 }
 
 data "template_file" "delete" {
   template = "${file("delete_repo.tmpl")}"
+
   vars = {
-    git_token = "${var.git_token}"
+    git_token    = "${var.git_token}"
     new_git_repo = "${var.new_git_repo}"
-    git_user = "${var.git_user}"
+    git_user     = "${var.git_user}"
   }
 }
 
@@ -21,12 +22,9 @@ resource "null_resource" "github_mgmt" {
   provisioner "local-exec" {
     command = "${data.template_file.create.rendered}"
   }
-  
-  provisioner "local-exec"{
-    when = "destroy"
-    command = "${data.template_file.delete.rendered}"
-    
-  }
-  
-}
 
+  provisioner "local-exec" {
+    when    = "destroy"
+    command = "${data.template_file.delete.rendered}"
+  }
+}
